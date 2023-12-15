13b915e5b95835b8dbba220bceb14102232d9473
honeybee <shelby.e.pagan@gmail.com>
2023-12-15 13:11:53 -0500

    reset dev branchh

--- routes/api-router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const apiCtrl = require("../controllers/movies-api.js");
-
-router.get("/", apiCtrl.index);
-router.get("/:id", apiCtrl.show);
-
-module.exports = router;
