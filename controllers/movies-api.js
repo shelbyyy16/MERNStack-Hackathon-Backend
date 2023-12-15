ed7004a82f564d688a3429fd5d713c87fc007e5c
honeybee <shelby.e.pagan@gmail.com>
2023-12-15 16:08:21 -0500

    controllers

--- controllers/movies-api.js
+++ controllers/movies-api.js
@@ -3,15 +3,15 @@ const axios = require('axios');
 
 async function index(req, res, next) {
   try {
-    const ratingsFilter = req.query.ratings;
-    const recommendations = await getMovies(ratingsFilter);
+    const catsFilter = req.query.cats;
+    const recommendations = await getMovies(catsFilter);
     res.status(200).json(recommendations);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 }
 
-async function getMovies(ratingsFilter) {
+async function getMovies() {
   const omdbApiKey = '6d50e8a9';
   const omdbApiUrl = `http://www.omdbapi.com/?s=cats&apikey=${omdbApiKey}`;
 
